64ce9372d15ca0d08d80f963f1709a74861b97d8
Hercinger Viktor <hercinger.viktor@gmail.com>
2024-04-29 13:18:22 +0200

    extended configuration with local configs and vim-options

--- init.lua
+++ init.lua
@@ -1,22 +1,5 @@
 -- Done with the tutorial https://www.youtube.com/watch?v=zHTeCSVAFNY&list=PLsz00TDipIffreIaUNk64KxTIkQaGguqn
 
--- Imported from old .vimrc file
-vim.cmd("set ts=2")
-vim.cmd("set modelines=1")
-vim.cmd("set sts=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set expandtab")
-vim.cmd("set ai")
-vim.cmd("set et")
-vim.cmd("set nu")
-vim.cmd("set ml")
-vim.cmd("set fo=ro")
-vim.cmd("set nohlsearch")
-
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-vim.keymap.set('n', '<leader>R', ':source ~/.config/nvim/init.lua<cr>', { desc = "Reload configuration" })
-
 -- Select package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -31,4 +14,5 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require('vim-options')
 require('lazy').setup("plugins")
---
+++ lua/vim-options.lua
@@ -0,0 +1,16 @@
+-- Imported from old .vimrc file
+vim.cmd("set ts=2")
+vim.cmd("set modelines=1")
+vim.cmd("set sts=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set expandtab")
+vim.cmd("set ai")
+vim.cmd("set et")
+vim.cmd("set nu")
+vim.cmd("set ml")
+vim.cmd("set fo=ro")
+vim.cmd("set nohlsearch")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.keymap.set('n', '<leader>R', ':source ~/.config/nvim/init.lua<cr>', { desc = "Reload configuration" })
