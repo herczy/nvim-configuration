50479681527df5f39419cc7ca52088a041ccb3ef
Hercinger Viktor <hercinger.viktor@gmail.com>
2024-05-02 21:42:31 +0200

    added tab as the autocompletion key

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -16,6 +16,21 @@ return {
 			local cmp = require("cmp")
 			require("luasnip.loaders.from_vscode").lazy_load()
 
+			function smartTab()
+				local win = vim.api.nvim_get_current_win()
+				local _, col = unpack(vim.api.nvim_win_get_cursor(win))
+				local line = vim.api.nvim_get_current_line()
+				local ch = line:sub(col, col + 1)
+
+				if ch:len() == 0 or ch:match("^[%s.]$") then
+					vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<tab>", true, true, true), "n", false)
+				else
+					cmp.complete()
+				end
+			end
+
+			vim.keymap.set("i", "<TAB>", smartTab, {})
+
 			cmp.setup({
 				snippet = {
 					-- REQUIRED - you must specify a snippet engine
@@ -38,7 +53,7 @@ return {
 					["<esc>"] = cmp.mapping.abort(),
 					-- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<TAB>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<TAB>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
@@ -49,6 +64,10 @@ return {
 				}, {
 					{ name = "buffer" },
 				}),
+				completion = {
+					keyword_length = 3,
+					autocomplete = false,
+				},
 			})
 
 			-- Set configuration for specific filetype.
@@ -59,25 +78,6 @@ return {
 					{ name = "buffer" },
 				}),
 			})
-
-			-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-			-- cmp.setup.cmdline({ '/', '?' }, {
-			--   mapping = cmp.mapping.preset.cmdline(),
-			--   sources = {
-			--     { name = 'buffer' }
-			--   }
-			-- })
-
-			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-			-- cmp.setup.cmdline(':', {
-			--   mapping = cmp.mapping.preset.cmdline(),
-			--   sources = cmp.config.sources({
-			--     { name = 'path' }
-			--   }, {
-			--     { name = 'cmdline' }
-			--   }),
-			--   matching = { disallow_symbol_nonprefix_matching = false }
-			-- })
 		end,
 	},
 }
