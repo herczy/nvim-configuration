e094ad94e262aa4ede0c96b424031268e7da0738
Hercinger Viktor <hercinger.viktor@gmail.com>
2024-05-04 16:40:24 +0200

    additional bindings file separated

--- lua/additional-bindings.lua
+++ lua/additional-bindings.lua
@@ -1,6 +1,6 @@
 local which_key = require("which-key")
 
 which_key.register({
-  j = { [[ <cmd>cn<cr> ]], "Next error" },
-  k = { [[ <cmd>cp<cr> ]], "Previous error" },
+	j = { [[ <cmd>cn<cr> ]], "Next error" },
+	k = { [[ <cmd>cp<cr> ]], "Previous error" },
 }, { prefix = "g" })
