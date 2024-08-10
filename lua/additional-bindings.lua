6e05d9cd5d18c362c622d58815fbd928781f801b
Hercinger Viktor <hercinger.viktor@gmail.com>
2024-08-10 17:21:05 +0200

    added key combination to close all buffers but the current one

--- lua/additional-bindings.lua
+++ lua/additional-bindings.lua
@@ -3,4 +3,5 @@ local which_key = require("which-key")
 which_key.add({
 	{ "<leader>gj", [[ <cmd>cn<cr> ]], desc = "Next error" },
 	{ "<leader>gk", [[ <cmd>cp<cr> ]], desc = "Previous error" },
+  { "<leader>BD", [[ <cmd>%bd|e#<cr> ]], desc = "Delete all buffers but current" },
 })
